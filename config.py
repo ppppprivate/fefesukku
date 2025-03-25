398b74e88510236220886ae3abef3e80b15ae6d6
fefefe12 <cookiesxd419@gmail.com>
2025-03-25 11:35:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/auntyxd12/XXSHEHZADIXX",
+    "https://github.com/fefefe12/fefeshezu",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
