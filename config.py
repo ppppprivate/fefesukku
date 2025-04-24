92663057c03bfa43f65ebf816ac77ef84e4d2706
ppppprivate <xdd28209@gmail.com>
2025-04-24 21:45:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/fefefe12/fefeshezu",
+    "https://github.com/ppppprivate/fefesukku",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
