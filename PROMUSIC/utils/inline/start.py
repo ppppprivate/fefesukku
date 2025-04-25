8852d67f6f45e9da18ab80a87f34ac005e9ed5a3
ppppprivate <xdd28209@gmail.com>
2025-04-25 19:23:17 +0530

    Update start.py

--- PROMUSIC/utils/inline/start.py
+++ PROMUSIC/utils/inline/start.py
@@ -18,8 +18,30 @@ def start_panel(_):
     return buttons
 
 
+
 # Private panel for inline buttons
 def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_12"], callback_data="gib_source")
+        ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+    ]
+    return buttons
+
+
+def private_panell(_):
     buttons = [
         [
             InlineKeyboardButton(
